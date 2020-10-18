10a2f8c7436e1367a57a6fce08430300c2b5344e
Adrián Les <lesauz@gmail.com>
2020-10-18 22:48:36 +0200

    View for registries
    Obtain existing registries
    Create new registry

--- assets/js/database.js
+++ assets/js/database.js
@@ -4,23 +4,52 @@ const sqlite = require('sqlite3');
 const dbName = 'registry.db';
 
 var db = null;
+var openPromise = null;
+
+var _allQuery = function (query, params)
+{
+    params = params || [];
+    return _toPromiseWhenOpen(function (resolve, reject) {
+        db.all(query, params, function (error, rows) {
+            if (null === error) {
+                resolve(rows);
+            } else {
+                reject(error);
+            }
+        });
+    });
+};
 
 var close = function ()
 {
     console.debug('closing db');
-    return new Promise(function (resolve, reject) {
-        if (db !== null) {
-            db.close(function (error) {
-                db = null;
-                if (null === error) {
-                    resolve();
-                } else {
-                    reject(error);
-                }
-            });
-        } else {
-            resolve();
-        }
+    return _toPromiseWhenOpen(function (resolve, reject) {
+        db.close(function (error) {
+            db = null;
+            if (null === error) {
+                resolve();
+            } else {
+                reject(error);
+            }
+        });
+    });
+};
+
+var getRegistries = function (isAsc)
+{
+    var order = isAsc ? 'ASC' : 'DESC';
+    return _allQuery(
+        "SELECT id, name, DATETIME(creation_date, 'localtime') AS creationDate, DATETIME(modification_date, 'localtime') AS modificationDate FROM registry ORDER BY creation_date " + order
+    );
+};
+
+var insertRegistry = function (name)
+{
+    return _runQuery(
+        "INSERT INTO registry (name, creation_date, modification_date) VALUES ($name, DATETIME('now'), DATETIME('now'))",
+        {$name: name}
+    ).then(function (stmt) {
+        return stmt.lastID;
     });
 };
 
@@ -30,8 +59,9 @@ var open = function ()
         close();
     }
     console.debug('opening db');
-    return new Promise(function (resolve, reject) {
+    openPromise = new Promise(function (resolve, reject) {
         var aux = new sqlite.Database(dbName, sqlite.OPEN_READWRITE, function (error) {
+            openPromise = null;
             if (null === error) {
                 db = aux;
                 resolve(db);
@@ -40,9 +70,43 @@ var open = function ()
             }
         });
     });
+    return openPromise;
+};
+
+var _runQuery = function (query, params)
+{
+    params = params || [];
+    return _toPromiseWhenOpen(function (resolve, reject) {
+        db.run(query, params, function (error) {
+            if (null === error) {
+                resolve(this);
+            } else {
+                reject(error);
+            }
+        });
+    });
+};
+
+var _toPromiseWhenOpen = function (func)
+{
+    return new Promise(function (resolve, reject) {
+        if (db === null) {
+            if (openPromise === null) {
+                reject('The database is not open');
+            } else {
+                openPromise.then(function () {
+                    func(resolve, reject);
+                });
+            }
+        } else {
+            func(resolve, reject);
+        }
+    });
 };
 
 export {
     open,
+    getRegistries,
+    insertRegistry,
     close
 };
--- assets/js/registries.js
+++ assets/js/registries.js
@@ -1,7 +1,58 @@
 console.debug('module registries.js is loading')
 
 import {
+    getRegistries as dbGetRegistries,
     insertRegistry as dbInsertRegistry
 } from './database.js';
 
 var tbody = document.getElementById('data-registries');
+var button = document.getElementById('insertRegistryButton');
+var input = document.getElementById('insertRegistryInput');
+button.addEventListener('click', function (event) {
+    var name = input.value;
+    dbInsertRegistry(input.value).then(function (registryId) {
+        reloadTable();
+        input.value = null;
+    });
+});
+
+var reloadTable = function ()
+{
+    dbGetRegistries().then(function (registries) {
+        while (tbody.firstChild) {
+            tbody.removeChild(tbody.firstChild);
+        }
+        for (var i = 0; i < registries.length; i++) {
+            createTableRow(registries[i]);
+        }
+    });
+};
+
+var createTableRow = function (registry)
+{
+    var tr = document.createElement('tr');
+    var td = document.createElement('td', {scope: 'row'});
+    td.textContent = registry.id;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.textContent = registry.name;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.textContent = registry.creationDate;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.textContent = registry.modificationDate;
+    tr.appendChild(td);
+    tr.addEventListener('click', function (event) {
+        onTableRowClickEvent(event, registry.id);
+    });
+    tbody.appendChild(tr);
+};
+
+var onTableRowClickEvent = function (event, registryId)
+{
+    //TODO:
+    console.log('tr with id ' + registryId + ' was clicked!');
+};
+
+reloadTable();
