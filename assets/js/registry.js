84d523c6d4ff2ccb31f7d0fb11576e3db370b4db
Adrián Les <lesauz@gmail.com>
2020-10-20 16:58:01 +0200

    New field validation for new registry row form

--- assets/js/registry.js
+++ assets/js/registry.js
@@ -9,6 +9,7 @@ import { showView as showRegistriesView } from './registries.js';
 import { showView as showReportView } from './report.js';
 
 const viewId = 'view-registry';
+const htmlClassInvalidControl = 'is-invalid';
 
 var currentRegistryId = null;
 var registriesMenuItem = document.getElementById(viewId).querySelector('.menu-item[data-r-target="registries"]');
@@ -26,33 +27,14 @@ var productInput = document.getElementById('insertRowProductInput');
 var quantityInput = document.getElementById('insertRowQuantityInput');
 var amountInput = document.getElementById('insertRowAmountInput');
 addRowButton.addEventListener('click', function (event) {
-    var row = {
-        registryId: currentRegistryId,
-        product: productInput.value,
-        quantity: quantityInput.value,
-        amount: amountInput.value
+    var row = validateForm();
+    if (row !== null) {
+        dbInsertRow(row).then(function (rowId) {
+            reloadTable();
+            initializeForm();
+        });
     }
-    //TODO: validation for quantity and amount
-    dbInsertRow(row).then(function (rowId) {
-        reloadTable();
-        productInput.value = null;
-        quantityInput.value = null;
-        amountInput.value = null;
-        productInput.focus();
-    });
 });
-
-var reloadTable = function ()
-{
-    dbGetRegistryRows(currentRegistryId).then(function (rows) {
-        while (tbody.firstChild) {
-            tbody.removeChild(tbody.firstChild);
-        }
-        for (var i = 0; i < rows.length; i++) {
-            createTableRow(rows[i]);
-        }
-    });
-};
 
 var createTableRow = function (registryRow, pos)
 {
@@ -72,12 +54,78 @@ var createTableRow = function (registryRow, pos)
     tbody.appendChild(tr);
 };
 
+var initializeForm = function ()
+{
+    productInput.value = null;
+    productInput.classList.remove(htmlClassInvalidControl);
+    quantityInput.value = null;
+    quantityInput.classList.remove(htmlClassInvalidControl);
+    amountInput.value = null;
+    amountInput.classList.remove(htmlClassInvalidControl);
+    productInput.focus();
+};
+
+var reloadTable = function ()
+{
+    dbGetRegistryRows(currentRegistryId).then(function (rows) {
+        while (tbody.firstChild) {
+            tbody.removeChild(tbody.firstChild);
+        }
+        for (var i = 0; i < rows.length; i++) {
+            createTableRow(rows[i]);
+        }
+    });
+};
+
 var showView = function (registryId)
 {
     currentRegistryId = registryId;
+    initializeForm();
     reloadTable();
     transitionToView(viewId);
     productInput.focus();
+};
+
+var validateForm = function ()
+{
+    productInput.classList.remove(htmlClassInvalidControl);
+    quantityInput.classList.remove(htmlClassInvalidControl);
+    amountInput.classList.remove(htmlClassInvalidControl);
+    if (quantityInput.value === '') {
+        quantityInput.value = 0;
+    }
+    if (amountInput.value === '') {
+        amountInput.value = 0;
+    }
+    var row = {
+        registryId: currentRegistryId,
+        product: productInput.value,
+        quantity: Number(quantityInput.value),
+        amount: Number(amountInput.value)
+    }
+    var error = false;
+    if (row.product.length !== 10) {
+        productInput.classList.add(htmlClassInvalidControl);
+        if (!error) {
+            productInput.focus();
+        }
+        error = true;
+    }
+    if (!Number.isInteger(row.quantity) || row.quantity < 0) {
+        quantityInput.classList.add(htmlClassInvalidControl);
+        if (!error) {
+            quantityInput.focus();
+        }
+        error = true;
+    }
+    if (row.amount < 0) {
+        amountInput.classList.add(htmlClassInvalidControl);
+        if (!error) {
+            amountInput.focus();
+        }
+        error = true;
+    }
+    return (!error) ? row : null;
 };
 
 export {
